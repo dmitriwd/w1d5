92acb317497fe1a03c142329a130d47d668f429e
Andre <itstheandre@gmail.com>
2020-10-23 14:45:42 +0200

    This last part was fun

--- oop-fundamentals/index.js
+++ oop-fundamentals/index.js
@@ -110,34 +110,34 @@ class Student {
 
 // WebDevStudent = new Student + whatever new properties we want to add
 
-class WebDevStudent extends Student {
-  constructor(name, age, favoriteLab, position) {
-    super();
-    this.age = age * 7;
-    this.favoriteLab = favoriteLab;
-    this.position = position;
-    this.cash = 100000000;
-  }
+// class WebDevStudent extends Student {
+//   constructor(name, age, favoriteLab, position) {
+//     super();
+//     this.age = age * 7;
+//     this.favoriteLab = favoriteLab;
+//     this.position = position;
+//     this.cash = 100000000;
+//   }
 
-  hackWindows() {
-    console.log("Pff, easy peasy");
-  }
-}
+//   hackWindows() {
+//     console.log("Pff, easy peasy");
+//   }
+// }
 
-class DoesntDrawDice extends Student {
-  constructor() {
-    super();
-    this.drawDice = undefined;
-  }
-}
+// class DoesntDrawDice extends Student {
+//   constructor() {
+//     super();
+//     this.drawDice = undefined;
+//   }
+// }
 
-let student = new Student("Rosana", 21);
-console.log("student:", student);
-let student2 = new WebDevStudent("Marta", 17, "GreatestMoviesLab", 10000);
-console.log("student2:", student2);
-student2.drawDice();
-student2.hackWindows();
-console.clear();
+// let student = new Student("Rosana", 21);
+// console.log("student:", student);
+// let student2 = new WebDevStudent("Marta", 17, "GreatestMoviesLab", 10000);
+// console.log("student2:", student2);
+// student2.drawDice();
+// student2.hackWindows();
+// console.clear();
 
 class Hangman {
   constructor() {
@@ -179,9 +179,111 @@ class Game {
 
 const boardGame = new Game("javascript");
 
-console.log("boardGame:", boardGame);
-
 if (boardGame.word.guess("e")) {
 } else {
   boardGame.hangman.failedLetter();
 }
+
+console.clear();
+
+class Human {
+  constructor() {
+    this.lives = 1; // You Only Live Once
+  }
+
+  screamYolo() {
+    console.log(`YOLO`);
+  }
+}
+
+// NotSmartHumanBeing = new Human() + changing ScreamHYoto
+class NotSmartHumanBeing extends Human {
+  /* 
+  constructor() {
+    super()
+  }
+
+  // its hidden in the depths of javascript
+  */
+  screamYolo() {
+    console.log(`Heck YEAH. YOOOOOOOLOOOO`);
+  }
+}
+
+// SmartHumanBeing -> new Human() + name+ different screamYolo
+class SmartHumanBeing extends Human {
+  constructor(name) {
+    super();
+    this.name = name;
+    // this.screamYolo = undefined;
+  }
+
+  screamYolo() {
+    console.log(
+      `Listen, I am a smart human being. Smart human beings dont scream YOLO`
+    );
+  }
+}
+
+// WantsToBeSmarter -> new Human() + new SmartHumanBeing(name) + isStufying property + wantsToParty
+class WantsToBeSmarter extends SmartHumanBeing {
+  constructor(isStudying, name) {
+    super(name);
+    this.isStudying = isStudying;
+  }
+
+  wantsToParty() {
+    console.log("Not really. I need all my brain cells");
+  }
+}
+
+class DoesntHaveTimeToBeSmarter extends SmartHumanBeing {
+  /* 
+  constructor(name) {
+    super(name) -> to call human and smart human being
+  }
+  */
+  isBuildingFuture() {
+    return true;
+  }
+}
+
+class WebDevStudent extends WantsToBeSmarter {
+  constructor(isStudying, name, field) {
+    super(isStudying, name);
+    this.field = field;
+    this.isLikingWebDev = undefined;
+  }
+}
+
+class Ironhacker extends WebDevStudent {
+  constructor(name, favoriteLab) {
+    super(true, name, "frontend");
+    this.favoriteLab = favoriteLab;
+  }
+
+  graduate() {
+    console.log("On my way to!");
+  }
+}
+
+class WebDevRemoteClass {
+  constructor() {
+    this.room = [
+      new Ironhacker("querien", "Greatest Movie Lab"),
+      new Ironhacker("alex", "Greatest Movie Lab"),
+      new Ironhacker("gosia", "Greatest Movie Lab"),
+    ];
+  }
+}
+
+const querien = new Ironhacker("Querien", "Greatest Movie Lab");
+
+function returnsTrue() {
+  return true;
+}
+
+const ourClass = new WebDevRemoteClass();
+console.log("ourClass:", ourClass);
+
+ourClass.room.forEach((element) => element.screamYolo());
